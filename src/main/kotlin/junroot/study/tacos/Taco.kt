cca1c47adde32cd73f10713ef5ca1a42c31de695
junroot <junroot0909@gmail.com>
2023-03-14 23:07:16 +0900

    정보 보여주기

---
+++ src/main/kotlin/junroot/study/tacos/Taco.kt
@@ -0,0 +1,3 @@
+package junroot.study.tacos
+
+data class Taco(val name: String? = null, val ingredients: List<String>? = null)
---
+++ src/main/kotlin/junroot/study/tacos/web/DesignTacoController.kt
@@ -0,0 +1,50 @@
+package junroot.study.tacos.web
+
+import junroot.study.tacos.Ingredient
+import junroot.study.tacos.Taco
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@RequestMapping("/design")
+@Controller
+class DesignTacoController {
+
+	companion object {
+		val log = LoggerFactory.getLogger(DesignTacoController::class.java)
+	}
+
+	@GetMapping
+	fun showDesignForm(model: Model): String {
+		val ingredients = listOf(
+			Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+			Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+			Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+			Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+			Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+			Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+			Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+			Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+			Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+			Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+		)
+
+		val types = Ingredient.Type.values()
+		for (type in types) {
+			model.addAttribute(
+				type.name.lowercase(),
+				filterByType(ingredients, type)
+			)
+		}
+
+		model.addAttribute("taco", Taco())
+
+		return "design"
+	}
+
+	private fun filterByType(ingredients: List<Ingredient>, type: Ingredient.Type): List<Ingredient> {
+		return ingredients.filter { it.type == type }
+	}
+}
